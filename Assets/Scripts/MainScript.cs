84a923a15ef1e2d8f093ce3744b0ebdfb1329fa0
MiroslavZ <zenkov-miroslav@mail.ru>
2020-10-19 02:12:26 +0500

    Added UI

--- Assets/Scripts/MainScript.cs
+++ Assets/Scripts/MainScript.cs
@@ -12,13 +12,15 @@ public class MainScript : MonoBehaviour
     public float BoxFriction = 0.3f;
     public float RailFriction = 0.3f;
     public float Mass = 1;
-    public float SupportReactionForce = 1;
+    float SupportReactionForce = 0;
     public float Gravity = -9.81f;
     //
     PhysicMaterial BoxPhysMaterial;
     PhysicMaterial RailPhysMaterial;
     Rigidbody boxRigidBody;
     RailScript railScript;
+    public bool PhysicsActivated = true;
+    public bool IsAxisOffsetPhysics = false;
 
     private void Awake()
     {
@@ -40,20 +42,66 @@ public class MainScript : MonoBehaviour
 
     void FixedUpdate()
     {
+        if (PhysicsActivated)
+        {
+            if (IsAxisOffsetPhysics)
+                AxisPhysics();
+            else
+                VectorPhysics();
+        }
+    }
+
+    void DrawForceRays()
+    {
+        var temp = Box.transform.position.x < RailCenter.position.x ? 1 : -1;
+        var railAngle = Vector3.Angle(Rail.transform.right, Vector3.right);
+        var G = new Vector3(0, Gravity, 0) * Mass;
+        Debug.DrawRay(Box.transform.position, G, Color.yellow);
+        var N = Quaternion.AngleAxis(railAngle * temp, Vector3.forward) * G * Mass * Mathf.Cos(railAngle);
+        Debug.DrawRay(Box.transform.position, N, Color.red);
+        var FF = Quaternion.AngleAxis(90 * temp, Vector3.back) * N * RailFriction * temp;
+        Debug.DrawRay(Box.transform.position, FF, Color.blue);
+        var Ma = G + N + FF;
+        Debug.DrawRay(Box.transform.position, Ma, Color.green);
+    }
+
+    void VectorPhysics()
+    {
+        Debug.Log("VectorPhysics enabled!");
         var temp = Box.transform.position.x < RailCenter.position.x ? 1 : -1;
         if (railScript.BoxIsOnTheRail)
         {
             var railAngle = Vector3.Angle(Rail.transform.right, Vector3.right);
             var G = new Vector3(0, Gravity, 0) * Mass;
             var N = Quaternion.AngleAxis(railAngle * temp, Vector3.forward) * G * Mass * Mathf.Cos(railAngle);
-            var FF = Quaternion.AngleAxis(90 * temp, Vector3.back) * N * RailFriction * temp;
+            var temp1 = Box.transform.position.x < RailCenter.position.x ? -1 : 1;
+            var FF = Quaternion.AngleAxis(90 * -1 * temp1 * temp, Vector3.back) * N * RailFriction * temp;
             var Ma = G + N + FF;
-            //Debug.DrawRay(Box.transform.position, Ma, Color.cyan, 0.3f);
             boxRigidBody.AddForce(Ma, ForceMode.Acceleration);
         }
         else
             boxRigidBody.AddForce(new Vector3(0, Gravity, 0), ForceMode.Acceleration);
-        //DrawForceRays();
+    }
+
+    bool tempFlag = false;
+    void AxisPhysics()
+    {
+        Debug.Log("AxisPhysics enabled!");
+        var temp = Box.transform.position.x < RailCenter.position.x ? 1 : -1;
+        if (railScript.BoxIsOnTheRail&&tempFlag)
+        {
+            var railAngle = Vector3.Angle(Rail.transform.right, Vector3.right);
+            SupportReactionForce = Mass * Gravity * Mathf.Cos(railAngle);
+            var tempx = SupportReactionForce * RailFriction * Mathf.Cos(railAngle)
+                - SupportReactionForce * Mathf.Sin(railAngle);
+            var tempy = Mass * Gravity - SupportReactionForce * RailFriction * Mathf.Sin(railAngle)
+                - SupportReactionForce * Mathf.Cos(railAngle);
+            Debug.DrawRay(Box.transform.position, new Vector3(tempx * temp * 1.0f, tempy, 0), Color.yellow, 0.3f);
+            boxRigidBody.AddForce(new Vector3(tempx * temp*1.0f, tempy, 0), ForceMode.Acceleration);
+            tempFlag = true;
+        }
+        else
+            boxRigidBody.AddForce(new Vector3(0, Gravity, 0), ForceMode.Acceleration);
     }
 
     void UpdateFriction(PhysicMaterial m, float newFrictionValue)
@@ -65,52 +113,5 @@ public class MainScript : MonoBehaviour
     void UpdateMass()
     {
         boxRigidBody.mass = Mass;
-    }
-
-    void DrawForceRays()
-    {
-        var railAngle = Vector3.Angle(Rail.transform.right, Vector3.right);
-        var G = new Vector3(0, Gravity, 0) * Mass;
-        Debug.DrawRay(Box.transform.position, G, Color.yellow);
-        var N = Quaternion.AngleAxis(railAngle, Vector3.forward) * G * Mass * Mathf.Cos(railAngle);
-        Debug.DrawRay(Box.transform.position, N, Color.red);
-        var FF = Quaternion.AngleAxis(90, Vector3.back) * N * RailFriction;
-        Debug.DrawRay(Box.transform.position, FF, Color.blue);
-        var Ma = G + N + FF;
-        Debug.DrawRay(Box.transform.position, Ma, Color.green);
-    }
-
-    void PhysicsV2()
-    {
-        if (railScript.BoxIsOnTheRail)
-        {
-            var railAngle = Vector3.Angle(Rail.transform.right, Vector3.right);
-            var G = new Vector3(0, Gravity, 0) * Mass;
-            var N = Quaternion.AngleAxis(railAngle, Vector3.forward) * G * Mass * Mathf.Cos(railAngle);
-            var FF = Quaternion.AngleAxis(90, Vector3.back) * N * RailFriction;
-            var Ma = G + N + FF;
-            //Debug.DrawRay(Box.transform.position, Ma, Color.cyan, 0.3f);
-            boxRigidBody.AddForce(Ma, ForceMode.Acceleration);
-        }
-        else
-            boxRigidBody.AddForce(new Vector3(0, Gravity, 0), ForceMode.Acceleration);
-    }
-
-    void PhysicsV1()
-    {
-        if (railScript.BoxIsOnTheRail)
-        {
-            var railAngle = Vector3.Angle(Rail.transform.right, Vector3.right);
-            SupportReactionForce = Mass * Gravity * Mathf.Cos(railAngle);
-            var tempx = SupportReactionForce * RailFriction * Mathf.Cos(railAngle) 
-                - SupportReactionForce * Mathf.Sin(railAngle);
-            var tempy = Mass * Gravity - SupportReactionForce * RailFriction * Mathf.Sin(railAngle)
-                - SupportReactionForce * Mathf.Cos(railAngle);
-            Debug.DrawRay(Box.transform.position, new Vector3(tempx, tempy, 0), Color.yellow, 0.3f);
-            boxRigidBody.AddForce(new Vector3(tempx, tempy, 0), ForceMode.Acceleration);
-            //DrawForceRays();
-        }
-        else
-            boxRigidBody.AddForce(new Vector3(0, Gravity, 0), ForceMode.Acceleration);
     }
 }
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    MainScript mainScript;
+    GameObject Box;
+    public InputField Gravity;
+    public InputField Mass;
+    public InputField BFriction;
+    public InputField RFriction;
+    public Text text;
+
+    private void Awake()
+    {
+        Box = GameObject.Find("Box");
+        mainScript = GetComponent<MainScript>();                    
+        Gravity.text=mainScript.Gravity.ToString();         
+        Mass.text = mainScript.Mass.ToString();
+        BFriction.text = mainScript.BoxFriction.ToString();
+        RFriction.text = mainScript.RailFriction.ToString();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (mainScript.IsAxisOffsetPhysics)
+            text.text = "AxisOffset";
+        else
+            text.text = "VectorSum";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float g;
+        bool c1 = float.TryParse(Gravity.text, out g);
+        float m;
+        bool c2 = float.TryParse(Mass.text, out m);
+        float bf;
+        bool c3 = float.TryParse(BFriction.text, out bf);
+        float rf;
+        bool c4 = float.TryParse(RFriction.text, out rf);
+        if (c1)
+            mainScript.Gravity = g;
+        if (c2)
+            mainScript.Mass = m;
+        if (c3)
+            mainScript.BoxFriction = bf;
+        if (c4)
+            mainScript.RailFriction = rf;
+    }
+
+    public void RestartScene() 
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void StartMoving()
+    {
+        float g;
+        bool c1 = float.TryParse(Gravity.text, out g);
+        float m;
+        bool c2 = float.TryParse(Mass.text, out m);
+        float bf;
+        bool c3 = float.TryParse(BFriction.text, out bf);
+        float rf;
+        bool c4 = float.TryParse(RFriction.text, out rf);
+        if (c1)
+            mainScript.Gravity=g;
+        if (c2)
+            mainScript.Mass=m;
+        if (c3)
+            mainScript.BoxFriction=bf;
+        if (c4)
+            mainScript.RailFriction=rf;
+        mainScript.PhysicsActivated = true;
+    }
+
+    public void SwitchPhysicsType()
+    {
+        if (mainScript.IsAxisOffsetPhysics)
+            text.text = "VectorSum";
+        else
+            text.text = "AxisOffset";
+        mainScript.IsAxisOffsetPhysics = !mainScript.IsAxisOffsetPhysics;
+    }
+
+    void ChoosePosition(float x,float y)
+    {
+        Box.transform.position = new Vector3(x, y, 0);
+    }
+
+    public void ChoosePositionTopLeft()
+    {
+        ChoosePosition(-0.25f, 2.28f);
+    }
+
+    public void ChoosePositionTopRight()
+    {
+        ChoosePosition(0.25f, 2.28f);
+    }
+
+    public void ChoosePositionBottomLeft()
+    {
+        ChoosePosition(-0.25f, 1.78f);
+    }
+
+    public void ChoosePositionBottomRight()
+    {
+        ChoosePosition(0.25f, 1.78f);
+    }
+}
